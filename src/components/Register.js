ba26382b5804c185a976307248a0c93ad909bc4c
Pankaj singh <pankaj.koranga.098@gmail.com>
2022-12-25 19:55:53 +0530

    Update Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -16,7 +16,7 @@ const Register = () => {
         e.preventDefault();
         const x = {
             method:"post",
-            url:"https://fitness-web.onrender.com/subs",
+            url:"https://fitness-back-mw8a-yb7xi5npk-0pain01.vercel.app/subs",
             data: {
                 name,
                 email
